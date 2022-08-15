fcb0ee9c4f33905d3eb1f4c672ef89155d118cf9
uhaseeb <uhaseeb731@gmail.com>
2022-08-15 16:26:48 +0500

    blog app project done with models

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.caption}"
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email_address = models.EmailField()
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return f"{self.full_name()}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    image_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    date = models.DateField(auto_now=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True, related_name="posts")
+    tags = models.ManyToManyField("Tag")
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from . import constants
+from .models import Post
 
-# Create your views here.
+
+def starting_page(request):
+    latest_post = Post.objects.all().order_by("-date")[:3]
+    return render(request, 'index.html', {'latest_post': latest_post})
+
+
+def all_posts(request):
+    all_post = Post.objects.all().order_by("-date")
+    return render(request, 'all-post.html', {"all_post": all_post})
+
+
+def post_detail(request, slug):
+    identified_post = get_object_or_404(Post, slug=slug)
+    post_tags = identified_post.tags.all()
+    return render(request, 'detail-post.html', {'identified_post': identified_post,
+                                                'post_tags': post_tags})
+
+
